22d81d2e4cd2e3864b0953f87186e417ccc9675d
Administrator <Administrator@DESKTOP-IKD2D8I>
2024-02-19 21:06:02 +0200

    Add project files.

---
+++ lab1ex4/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Scrieti un program care va afisa semnul unui numar citit de la tastatura
+• Daca numarul este pozitiv, va afisa “+”
+• Daca numarul este negativ, va afisa “-”
+• Daca numarul este 0, va afisa “0”*/
+
+Console.WriteLine("Introduceti numarul");
+int Numar = Convert.ToInt32(Console.ReadLine());
+if (Numar > 0)
+{
+    Console.WriteLine("+");
+}
+else if (Numar < 0)
+{
+    Console.WriteLine("-");
+}
+else if (Numar == 0)
+{
+    Console.WriteLine("0");
+}
